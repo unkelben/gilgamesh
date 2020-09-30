e10734fb082b5b996facdb77f1c25d8a64cf9aac
William L'Eriger <espermentalout@gmail.com>
2020-09-29 22:55:44 -0400

    William: Targets for Needles and Dragging

---
+++ Gilgamesh/Assets/William/Scripts/Dragging.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dragging : MonoBehaviour
+{
+
+    public bool isDragging;
+    Vector2 mousePosition;
+
+   
+
+    public void OnMouseDown()
+    {
+        isDragging = true;
+    }
+
+    public void OnMouseUp()
+    {
+        isDragging = false;
+    }
+
+    void Update()
+    {
+        mousePosition = Input.mousePosition;
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition) - transform.position;
+ 
+        if (isDragging)
+        {
+            //Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+            transform.Translate(mousePosition);
+        }
+    }
+
+
+}
+
---
+++ Gilgamesh/Assets/William/Scripts/Pierce.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pierce : MonoBehaviour
+{
+
+    public float pushValueX;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Target")
+        {
+            Destroy(collision.gameObject);
+            Destroy(this.gameObject);
+
+            Vector2 pushBack = collision.transform.parent.position;
+            pushBack -= new Vector2 (pushValueX,0);
+            collision.transform.parent.position = pushBack;
+        }
+    }
+}
