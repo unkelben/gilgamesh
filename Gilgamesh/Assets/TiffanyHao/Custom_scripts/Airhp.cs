be3283ae7c673fa19a5ce397a4e2fe571c039b7a
tiffhc <46658494+tiffhc@users.noreply.github.com>
2020-09-30 22:28:37 -0400

    Testing health bar

---
+++ Gilgamesh/Assets/TiffanyHao/Custom_scripts/Airhp.cs
@@ -0,0 +1,34 @@
+﻿using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class Airhp : MonoBehaviour
+{
+    public int curAir = 0; 
+    public int maxAir = 100;
+
+    public int decreaseRate = 5;
+
+    //to check 
+    //public Airbar airb; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        curAir = maxAir; 
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        
+    }
+
+    public void DecreaseAir()
+    {
+        curAir -= decreaseRate;
+
+        //airb.setAir(curAir); 
+    }
+}
