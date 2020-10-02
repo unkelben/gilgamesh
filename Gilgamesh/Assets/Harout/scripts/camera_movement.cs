c00bb1245355c843e341a2e1c4cf54ebc28f8542
Harout77 <harout@live.ca>
2020-10-02 16:27:36 -0400

    Enkidu joins the journey

---
+++ Gilgamesh/Assets/Harout/scripts/camera_movement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camera_movement : MonoBehaviour
+{
+
+
+    public Transform target;
+    public float smoothing;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(transform.position != target.position)
+        { 
+            Vector3 targetPosition = new Vector3(target.position.x,
+                                                target.position.y,
+                                                transform.position.z);
+
+            transform.position = Vector3.Lerp(transform.position,
+                                              targetPosition, smoothing);
+        }
+    }
+}
