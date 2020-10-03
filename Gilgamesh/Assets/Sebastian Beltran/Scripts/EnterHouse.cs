e37b00c814cd46ca7f13ff32e5b616abdb94fa3b
sfba96 <31808176+sfba96@users.noreply.github.com>
2020-10-03 11:10:45 -0400

    Scene Transition
    Load second scene when enkidu enters the house

---
+++ Gilgamesh/Assets/Sebastian Beltran/Scripts/EnterHouse.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EnterHouse : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        SceneManager.LoadScene("Enkidu and Shepherds");
+    }
+}
