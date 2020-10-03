fa99cdc9457bbf0f220f7b35969f385ca200fa5d
Harout77 <harout@live.ca>
2020-10-03 19:30:18 -0400

    Added an endgame and a cutscene

---
+++ Gilgamesh/Assets/Harout/scripts/change_scene.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class change_scene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("vignette_1");
+    }
+
+
+
+
+
+}
---
+++ Gilgamesh/Assets/Harout/scripts/dialogue_trigger.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dialogue_trigger : MonoBehaviour
+{
+
+
+
+
+    public Dialogue dialogue;
+
+    public void TriggerDialogue ()
+    {
+        FindObjectOfType<dialogue_manager>().StartDialogue(dialogue);
+    }
+
+}
