9d1a86e4f875bd128937240bf742fbccba018965
Harout77 <harout@live.ca>
2020-10-03 12:13:37 -0400

    Gilgamesh learned a new ability !

---
+++ Gilgamesh/Assets/Harout/scripts/tree.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tree : MonoBehaviour
+{
+
+    private Animator anim;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Break()
+    {
+        anim.SetBool("break", true);
+        StartCoroutine(breakCo());
+    }
+
+
+    IEnumerator breakCo()
+    {
+        yield return new WaitForSeconds(2F);
+        this.gameObject.SetActive(false);
+    }
+
+}
