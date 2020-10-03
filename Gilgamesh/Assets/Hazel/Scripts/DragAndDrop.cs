bb4475b64da08368f63516fe37280dd5a1692a11
Hazel Thexton <hazelthexton@videotron.ca>
2020-10-03 03:38:40 -0400

    Added assets and dragging script

---
+++ Gilgamesh/Assets/Hazel/Scripts/DragAndDrop.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class DragAndDrop : MonoBehaviour
+{
+    bool canMove;
+    bool dragging;
+    public Collider2D collider;
+    public Collider2D collider2;
+    void Start()
+    {
+        collider = GetComponent<Collider2D>();
+        collider2 = GetComponent<Collider2D>();
+        canMove = false;
+        dragging = false;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (collider == Physics2D.OverlapPoint(mousePos))
+            {
+                canMove = true;
+            }
+            else
+            {
+                canMove = false;
+            }
+            if (canMove)
+            {
+                dragging = true;
+            }
+
+
+        }
+        if (dragging)
+        {
+            this.transform.position = mousePos;
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            canMove = false;
+            dragging = false;
+        }
+    }
+
+}
---
+++ Gilgamesh/Assets/Snap.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Snap : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        //If the object hit is the player
+        if (Input.GetMouseButtonUp(0))
+        {
+            this.transform.position = new Vector3(0, 0, 0);
+        }
+    }
+}
