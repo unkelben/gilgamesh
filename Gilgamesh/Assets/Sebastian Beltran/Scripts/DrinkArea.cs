d0dac2a057558c7ae46e5979423dbd6e90fb00d2
sfba96 <31808176+sfba96@users.noreply.github.com>
2020-10-03 16:11:58 -0400

    Increased Velocity with each drink

--- Gilgamesh/Assets/Sebastian Beltran/Scripts/DrinkArea.cs
+++ Gilgamesh/Assets/Sebastian Beltran/Scripts/DrinkArea.cs
@@ -8,13 +8,16 @@ public class DrinkArea : MonoBehaviour
     public bool drinkIsInside;
     public int drinkCounter = 0;
     float initialPosition;
+    float velocityY;
     Vector2 gobletPos;
+
 
     // Start is called before the first frame update
     void Start()
     {
         gobletPos = new Vector2(goblet.transform.position.x, goblet.transform.position.y);
         initialPosition = gobletPos.y;
+        velocityY = -3f;
 
     }
 
@@ -22,10 +25,12 @@ public class DrinkArea : MonoBehaviour
     void Update()
     {
         gobletPos.y = goblet.transform.position.y;
+        goblet.GetComponent<Rigidbody2D>().velocity = new Vector2 (0f,velocityY);
         
         if (Input.GetKeyDown(KeyCode.Space) && drinkIsInside == true)
         {
             drinkCounter++;
+            velocityY = velocityY - 1;
         } else if (drinkIsInside == false && gobletPos.y < -1f)
         {
             gobletPos.y = initialPosition;
