368ebeb454481b39c33495e5c31ff087af23daeb
sfba96 <31808176+sfba96@users.noreply.github.com>
2020-10-03 16:23:18 -0400

    A bit of cleanup

--- Gilgamesh/Assets/Sebastian Beltran/Scripts/DrinkArea.cs
+++ Gilgamesh/Assets/Sebastian Beltran/Scripts/DrinkArea.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class DrinkArea : MonoBehaviour
 {
+    //Pass Goblet
     [SerializeField] GameObject goblet;
+
+    //Declare boolean that checks if the goblet is inside the drink area
+    //Initialize the count of drinks to 0
+    //Declare variable to store initial goblet position
+    //Declare velocity for goblet
+    //Declare vector 2 for goblet position
     public bool drinkIsInside;
     public int drinkCounter = 0;
     float initialPosition;
+    float velocityX = 0f;
     float velocityY;
     Vector2 gobletPos;
 
 
-    // Start is called before the first frame update
+    // Start
+    //initialize goblet position, velocity value and store Y position
     void Start()
     {
         gobletPos = new Vector2(goblet.transform.position.x, goblet.transform.position.y);
@@ -24,9 +33,14 @@ public class DrinkArea : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //update goblet position with current position
         gobletPos.y = goblet.transform.position.y;
-        goblet.GetComponent<Rigidbody2D>().velocity = new Vector2 (0f,velocityY);
-        
+
+        //pass velocity values to game object
+        goblet.GetComponent<Rigidbody2D>().velocity = new Vector2 (velocityX,velocityY);
+
+        //if space is hit when the goblet is inside the drink area then add 1 to the counter and increase velocity
+        //else reset position
         if (Input.GetKeyDown(KeyCode.Space) && drinkIsInside == true)
         {
             drinkCounter++;
@@ -39,6 +53,7 @@ public class DrinkArea : MonoBehaviour
         
     }
 
+    //OnTrigger events check wether the goblet is inside of the drink area or not
     public void OnTriggerEnter2D(Collider2D other)
     {
         if(other.tag == "Activator")
