2b553fc00baddf9a36c52fdf23e58ff162f4f9d5
sfba96 <31808176+sfba96@users.noreply.github.com>
2020-10-03 16:00:34 -0400

    Respawn Goblet if Missed

--- Gilgamesh/Assets/Sebastian Beltran/Scripts/DrinkArea.cs
+++ Gilgamesh/Assets/Sebastian Beltran/Scripts/DrinkArea.cs
@@ -4,21 +4,32 @@ using UnityEngine;
 
 public class DrinkArea : MonoBehaviour
 {
+    [SerializeField] GameObject goblet;
     public bool drinkIsInside;
     public int drinkCounter = 0;
+    float initialPosition;
+    Vector2 gobletPos;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        gobletPos = new Vector2(goblet.transform.position.x, goblet.transform.position.y);
+        initialPosition = gobletPos.y;
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        gobletPos.y = goblet.transform.position.y;
+        
         if (Input.GetKeyDown(KeyCode.Space) && drinkIsInside == true)
         {
             drinkCounter++;
+        } else if (drinkIsInside == false && gobletPos.y < -1f)
+        {
+            gobletPos.y = initialPosition;
+            goblet.transform.position = gobletPos;
         }
         
     }
