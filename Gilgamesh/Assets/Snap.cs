0b97989e1e01c31dcad9d9eb3184f5216ea1dda9
Hazel Thexton <hazelthexton@videotron.ca>
2020-10-03 18:02:10 -0400

    Snapping works now!!

--- Gilgamesh/Assets/Snap.cs
+++ Gilgamesh/Assets/Snap.cs
@@ -6,26 +6,14 @@ public class Snap : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
-    {
-        
+    {   
+
     }
 
-  private void OnCollisionStay2D(Collision2D collision)
-    {
-        //If the object hit is the player
-        if (Input.GetMouseButtonUp(0))
-        {
-            
-        }
-    }
-
-    private void OnCollisionEnter2D(Collision2D collision)
+    void OnCollisionStay2D(Collision2D collision)
     {
 
         this.transform.position = new Vector3(53, 0, 0);
-        if (collision.gameObject.tag == "Target")
-        {
-            
-        }
+        
     }
 }
