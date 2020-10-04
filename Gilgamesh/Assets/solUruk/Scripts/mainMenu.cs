b86c53260f2d7af22b673716c1eacbd3e0187f53
sorwu <solwpaul1@gmail.com>
2020-10-04 15:44:32 -0400

    added titlescreen, main menu w/ buttons and character selection (WIP)

---
+++ Gilgamesh/Assets/solUruk/Scripts/mainMenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class mainMenu : MonoBehaviour
+{
+    // Play as Gilgamesh
+    public void Gilgamesh()
+    {
+        SceneManager.LoadScene(1);
+    }
+    // Play as Enkidu
+    public void Enkidu()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void QuitGame()
+    {
+      Application.Quit();
+    }
+}
