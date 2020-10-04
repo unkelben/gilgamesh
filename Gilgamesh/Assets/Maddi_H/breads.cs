5b1c9eefc8a79d30630bd2c4832fc9798d2561f6
madslads <47368007+madslads@users.noreply.github.com>
2020-10-04 17:45:57 -0400

    counter+breadScripts

---
+++ Gilgamesh/Assets/Maddi_H/breads.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class breads : MonoBehaviour
+{
+    
+    public int breadValue = 1;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+           ScoreManager.instance.ChangeScore(breadValue);
+        }
+    }
+
+}
