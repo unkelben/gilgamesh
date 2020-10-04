0ff073ccc5833355038c30dc837bfda51d4ca147
Macey McCormick <red-rainbows@hotmail.com>
2020-10-04 11:48:43 -0400

    changing ChangeBackground code again

--- Gilgamesh/Assets/RobinMcCormick/Scripts/ChangeBackground.cs
+++ Gilgamesh/Assets/RobinMcCormick/Scripts/ChangeBackground.cs
@@ -4,36 +4,49 @@ using UnityEngine;
 
 public class ChangeBackground : MonoBehaviour
 {
-    public SpriteRenderer spriteRenderer;
-    public Sprite enkidu_bg1;
-    public Sprite enkidu_bg2;
-    public Sprite enkidu_bg3;
-    public Sprite enkidu_bg4;
-    public bool bg1Active = true;
-    public bool bg2Active = false;
-    public bool bg3Active = false;
-    public bool bg4Active = false;
+    public Sprite backgroundSprite2;
+    public Sprite backgroundSprite3;
+    public Sprite backgroundSprite4;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        SpriteRenderer bg1 = enkidu_bg1.Sprite.GetComponent<SpriteRenderer>();
-        bg1.enabled = true;
+      //  SpriteRenderer spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
 
-       // MeshRenderer mr1 = Maze1.gameObject.GetComponent<MeshRenderer>();
-       // mr1.enabled = true;
-       // Collider cr1 = Maze1.gameObject.GetComponent<Collider>();
-       // cr1.enabled = true;
-
-        bg1Active = true;
-        bg2Active = false;
-        bg3Active = false;
-        bg4Active = false;
 }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown("space"))
+        {
+            print("space key was pressed");
+            GameObject bg2 = new GameObject("enkidu_bg2", typeof(SpriteRenderer));
+            SpriteRenderer bg2Renderer = bg2.GetComponent<SpriteRenderer>();
+            bg2Renderer.sprite = backgroundSprite2;
+            bg2Renderer.sortingLayerName = "background";
+            bg2Renderer.sortingOrder = 2;
+        }
+
+        if (Input.GetKeyDown("e"))
+        {
+            print("e key was pressed");
+            GameObject bg3 = new GameObject("enkidu_bg3", typeof(SpriteRenderer));
+            SpriteRenderer bg3Renderer = bg3.GetComponent<SpriteRenderer>();
+            bg3Renderer.sprite = backgroundSprite3;
+            bg3Renderer.sortingLayerName = "background";
+            bg3Renderer.sortingOrder = 3;
+        }
+
+        if (Input.GetKeyDown("r"))
+        {
+            print("r key was pressed");
+            GameObject bg4 = new GameObject("enkidu_bg4", typeof(SpriteRenderer));
+            SpriteRenderer bg4Renderer = bg4.GetComponent<SpriteRenderer>();
+            bg4Renderer.sprite = backgroundSprite4;
+            bg4Renderer.sortingLayerName = "background";
+            bg4Renderer.sortingOrder = 4;
+        }
     }
 }
