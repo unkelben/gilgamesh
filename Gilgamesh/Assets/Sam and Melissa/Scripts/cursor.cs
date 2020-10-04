82bdb8e58187e86c38ff6e7203530c34534c4095
M33-Melissa <melissa_lim_17@hotmail.com>
2020-10-04 18:43:24 -0400

    Added cursors!

---
+++ Gilgamesh/Assets/Sam and Melissa/Scripts/cursor.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cursor : MonoBehaviour
+{
+    public Texture2D cursorArrow;
+
+    void Start()
+    {
+        Cursor.SetCursor(cursorArrow, Vector2.zero, CursorMode.ForceSoftware);
+    }
+}
