bc889b42e9b5f0e0ecb1c5684b98a74a3941101b
Mauricio Beltran Franco <mauricio.beltranfranco@live.fr>
2020-10-05 04:40:33 -0400

    Mauricio Game
    - Added prototype game.

---
+++ Gilgamesh/Assets/Mauricio Beltran Franco/Scripts/Boar.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boar : MonoBehaviour {
+    [SerializeField] float speed;
+    [SerializeField] Vector3 startPos;
+
+    [SerializeField] GameController gc;
+
+    bool start;
+    bool drink;
+
+    // Start is called before the first frame update
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (start) {
+            if (!drink) transform.Translate(Vector2.left * speed * Time.deltaTime);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.CompareTag("Enemy")) {
+            gc.Restart();
+        } else if (collision.name == "Pond") {
+            drink = true;
+            gc.Drink();
+        }
+    }
+
+    public void Restart() {
+        transform.position = startPos;
+        drink = false;
+        start = false;
+    }
+
+    public void StartGame() {
+        start = true;
+    }
+}
---
+++ Gilgamesh/Assets/Mauricio Beltran Franco/Scripts/GameController.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour {
+    [SerializeField] Boar boar;
+    [SerializeField] Hunter hunter;
+    [SerializeField] Player player;
+
+    [SerializeField] Image night;
+    [SerializeField] GameObject end;
+
+    int day = 1;
+
+    // Start is called before the first frame update
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    public void Restart() {
+        boar.Restart();
+        player.Restart();
+        hunter.Restart();
+    }
+
+    public void StartBoar() {
+        boar.StartGame();
+        player.StartGame();
+    }
+
+    public void Drink() {
+        player.Drink();
+    }
+
+    public void EndDay() {
+        hunter.Return();
+    }
+
+    public void NewDay() {
+        if (day == 3) {
+            StartCoroutine("LastDay");
+        } else {
+            day++;
+            StartCoroutine("NextDay");
+        }
+    }
+
+    IEnumerator NextDay() {
+        for (float i = 0; i <= 255; i++) {
+            night.color = new Color(0, 0, 0, i/255);
+            yield return new WaitForSeconds(3f / 255);
+        }
+        boar.Restart();
+        player.Restart();
+        for (float i = 255; i >= 0; i--) {
+            night.color = new Color(0, 0, 0, i / 255);
+            yield return new WaitForSeconds(3f / 255);
+        }
+        hunter.Restart();
+    }
+
+    IEnumerator LastDay() {
+        for (float i = 0; i <= 255; i++) {
+            night.color = new Color(0, 0, 0, i / 255);
+            yield return new WaitForSeconds(3f / 255);
+        }
+        end.SetActive(true);
+    }
+}
