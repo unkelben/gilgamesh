34d2d5eaaeb90a12c0f36368824b85d5c8798431
Macey McCormick <red-rainbows@hotmail.com>
2020-10-04 21:48:45 -0400

    adding animation to jug on click
    but it's doing a bit of a jig...

--- Gilgamesh/Assets/RobinMcCormick/Scripts/MouseOverWaterJug.cs
+++ Gilgamesh/Assets/RobinMcCormick/Scripts/MouseOverWaterJug.cs
@@ -5,17 +5,30 @@ using UnityEngine;
 public class MouseOverWaterJug : MonoBehaviour
 {
     public bool isMouseOverJug = false;
+    bool interactedWithJug = false;
+    Animator animator;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if (interactedWithJug == false)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Debug.Log("Interacted with jug.");
+                animator.SetBool("isPour", true);
+                interactedWithJug = true;
+            } else
+            {
+                animator.SetBool("isPour", false);
+            }
+        }
     }
 
     void OnMouseEnter()
