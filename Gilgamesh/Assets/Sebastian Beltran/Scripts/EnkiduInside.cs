cb99cc2817407f5ea187dd36b5e13e8a4d7f20de
sfba96 <31808176+sfba96@users.noreply.github.com>
2020-10-12 11:07:27 -0400

    First Second Scene Dialogues Working

--- Gilgamesh/Assets/Sebastian Beltran/Scripts/EnkiduInside.cs
+++ Gilgamesh/Assets/Sebastian Beltran/Scripts/EnkiduInside.cs
@@ -1,12 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Fungus;
 
 public class EnkiduInside : MonoBehaviour
 {
     [SerializeField] float speed;
+    [SerializeField] Flowchart flowchart;
     Rigidbody2D enkiduRigidBody;
     Animator enkiduAnimator;
+    bool allowMovement = false;
 
 
 
@@ -18,22 +21,36 @@ public class EnkiduInside : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Move();
+
+        allowMovement = flowchart.GetBooleanVariable("allowMovement");
+        
+         Move();
+        
+        
     }
 
     private void Move()
     {
-        float controlDir = Input.GetAxis("Horizontal");
-        Vector2 enkiduVel = new Vector2(controlDir * speed, enkiduRigidBody.velocity.y);
+        if (allowMovement == true)
+        {
+            float controlDir = Input.GetAxis("Horizontal");
+            Vector2 enkiduVel = new Vector2(controlDir * speed, enkiduRigidBody.velocity.y);
 
-        enkiduRigidBody.velocity = enkiduVel;
+            enkiduRigidBody.velocity = enkiduVel;
 
-        bool playerIsMoving = Mathf.Abs(enkiduRigidBody.velocity.x) > Mathf.Epsilon;
-       
-        enkiduAnimator.SetBool("Walking", playerIsMoving);
- 
+            bool playerIsMoving = Mathf.Abs(enkiduRigidBody.velocity.x) > Mathf.Epsilon;
 
-        Flip();
+            enkiduAnimator.SetBool("Walking", playerIsMoving);
+
+
+            Flip();
+        } else
+        {
+            Vector2 enkiduVel = new Vector2(0f,0f);
+
+            enkiduRigidBody.velocity = enkiduVel;
+            enkiduAnimator.SetBool("Walking", false);
+        }
     }
 
     private void Flip()
@@ -43,6 +60,18 @@ public class EnkiduInside : MonoBehaviour
         if (playerIsMoving)
         {
             transform.localScale = new Vector2 (Mathf.Sign(enkiduRigidBody.velocity.x) / 2, 0.5f);
+        }
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+
+        if (other.tag == "Meet Trigger")
+        {
+            flowchart.ExecuteBlock("Meet Dialogue");
+            
+
         }
 
     }
