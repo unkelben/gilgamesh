7664920380f7956f51f7da6f6196f31edcefcc8f
Macey McCormick <red-rainbows@hotmail.com>
2020-10-17 11:56:51 -0400

    adding test drag script

--- Gilgamesh/Assets/RobinMcCormick/Scripts/MouseOverCup.cs
+++ Gilgamesh/Assets/RobinMcCormick/Scripts/MouseOverCup.cs
@@ -10,8 +10,6 @@ public class MouseOverCup : MonoBehaviour
     public MouseOverWaterJug wJ;
     public ChangeBackground cB;
 
-    bool canMove;
-    bool dragging;
     public Collider2D cupCollider;
     public Collider2D enkiduCollider;
 
@@ -24,69 +22,23 @@ public class MouseOverCup : MonoBehaviour
     {
         sprite = GetComponent<SpriteRenderer>();
         sprite.color = Color.white;
-
-        canMove = false;
-        dragging = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        // Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
        // Debug.Log(mousePos);
         if (wJ.interactedWithJug == true)
-
-        //&& isMouseOverCup == true
         {
             //interactedWithCup == false   
-            if (Input.GetMouseButtonDown(0))
-            {
+         
                 
-              //  if (cupCollider == Physics2D.OverlapPoint(mousePos))
-              if (isMouseOverCup == true)
-                {
-                    Debug.Log("Clicked cup");
-                    sprite.color = Color.gray;
-                    canMove = true;
-                    Debug.Log("canMove = true");
-                    //   this.GetComponent<Snap>().dressed = false;
-                }
-                else
-                {
-                    canMove = false;
-                    sprite.color = Color.white;
-                }
-                if (canMove)
-                {
-                    dragging = true;
-                    Debug.Log("dragging = true");
-                }
-
-                if (dragging)
-                {
-                   
-                    sprite.transform.position = mousePos;
-                    Debug.Log("Transform to mouse pos");
-
-                }
-
-                if (Input.GetMouseButtonUp(0))
-                {
-                    canMove = false;
-                    dragging = false;
-                    Debug.Log("Done Dragging");
-                }
-
                 // animator.SetBool("cupIsMove", true);
                 // cB.interactionAmount = cB.interactionAmount++;
                 //  interactedWithCup = true;
-            }
-
-            
-
         }
     }
-        
 
     void OnMouseEnter()
     {
---
+++ Gilgamesh/Assets/RobinMcCormick/Scripts/TestDrag.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestDrag : MonoBehaviour
+{
+    private bool isDragging;
+    public bool dragActive;
+
+    public void OnMouseDown()
+    {
+        isDragging = true;
+    }
+
+    public void OnMouseUp()
+    {
+        isDragging = false;
+    }
+
+    void Update()
+    {
+        if (isDragging)
+        {
+            Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+            transform.Translate(mousePosition);
+        }
+    }
+}
