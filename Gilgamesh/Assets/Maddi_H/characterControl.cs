3d401ef8983aaf72c6127f139937b3f942a68089
Gordon Gray <31903322+Gmangray4@users.noreply.github.com>
2020-10-18 15:38:47 -0400

    Helped Maddi with her sprite issues.

--- Gilgamesh/Assets/Maddi_H/characterControl.cs
+++ Gilgamesh/Assets/Maddi_H/characterControl.cs
@@ -5,42 +5,72 @@ using UnityEngine;
 public class characterControl : MonoBehaviour
 {
     public float MovementSpeed = 1;
-  //  public float JumpForce = 1;
+    //  public float JumpForce = 1;
     public bool isGrounded = false;
 
-     Rigidbody2D _rigidbody;
+    //Gordon added:
+    private SpriteRenderer CharSprite;
+    //Gordon added:
+    public Sprite PackForest01_5;
+    //Gordon added:
+    public Sprite PackForest01_8;
+
+    //Gordon added
+    int Bread = 0;
+
+    Rigidbody2D _rigidbody;
     // Start is called before the first frame update
-     void Start()
+    void Start()
     {
-       
+
+        //Gordon added
+        CharSprite = gameObject.GetComponent<SpriteRenderer>();
+        //Gordon added
+        CharSprite.sprite = PackForest01_5;
+
+
     }
 
-    
+
     void Update()
     {
-       Jump();
-       Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
-       transform.position += movement * Time.deltaTime * MovementSpeed;
+        Jump();
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+        transform.position += movement * Time.deltaTime * MovementSpeed;
 
-       // changing sprite 
-    
+        // changing sprite 
+
     }
 
     void Jump()
     {
-        if(Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump"))
         {
-        gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, 5f), ForceMode2D.Impulse);
+            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, 5f), ForceMode2D.Impulse);
         }
     }
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        if(other.gameObject.CompareTag("bread")) 
+        if (other.gameObject.CompareTag("bread"))
         {
             Destroy(other.gameObject);
+            //Gordon added
+            Bread++;
+            //Gordon added:
+            CheckNumberOfEaten();
         }
     }
+
+    //Gordon added:
+    public void CheckNumberOfEaten(){
+        //Gordon added:
+        if (Bread >= 7)
+        {
+            //Gordon added:
+            CharSprite.sprite = PackForest01_8;
+        }
+}
 
     
 }
