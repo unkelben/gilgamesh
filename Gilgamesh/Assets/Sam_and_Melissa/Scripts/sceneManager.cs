b0abb45d1d3dae22f35000cade302ee5c1f4e53b
samjam <6parsam@gmail.com>
2020-10-17 20:51:43 -0400

    Updated scene transition code

--- Gilgamesh/Assets/Sam_and_Melissa/Scripts/sceneManager.cs
+++ Gilgamesh/Assets/Sam_and_Melissa/Scripts/sceneManager.cs
@@ -10,11 +10,17 @@ public class sceneManager : MonoBehaviour
 
     GameObject bubble1;
     GameObject bubble2;
+    GameObject bubble3;
+    GameObject bubble4;
+    GameObject bubble5;
+    GameObject bubble6;
     GameObject maskBubble;
     GameObject title;
 
     GameObject councillors;
     GameObject backdrop;
+
+    GameObject camera;
     Color bgColor;
     float trigger = 0f;
     int phase = 0;
@@ -22,7 +28,16 @@ public class sceneManager : MonoBehaviour
 
     public bool scene1started = false;
     public bool scene1over = false;
-    
+    int counter = 0;
+
+    Vector3 bubble1InitScale;
+    Vector3 bubble2InitScale;
+    Vector3 bubble3InitScale;
+    Vector3 bubble4InitScale;
+    Vector3 bubble5InitScale;
+    Vector3 bubble6InitScale;
+    Vector3 maskBubbleInitScale;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -33,9 +48,22 @@ public class sceneManager : MonoBehaviour
         scene3 = GameObject.Find("Scene3");
         bubble1 = GameObject.Find("Bubble1");
         bubble2 = GameObject.Find("Bubble2");
+        bubble3 = GameObject.Find("Bubble3");
+        bubble4 = GameObject.Find("Bubble4");
+        bubble5 = GameObject.Find("Bubble5");
+        bubble6 = GameObject.Find("Bubble6");
+        bubble1InitScale = bubble3.transform.localScale;
+        bubble2InitScale = bubble3.transform.localScale;
+        bubble3InitScale = bubble3.transform.localScale;
+        bubble4InitScale = bubble4.transform.localScale;
+        bubble5InitScale = bubble3.transform.localScale;
+        bubble6InitScale = bubble3.transform.localScale;
+        
         maskBubble = GameObject.Find("MaskBubble");
+        maskBubbleInitScale = maskBubble.transform.localScale;
         title = GameObject.Find("Title");
         councillors = GameObject.Find("council-dudes");
+        camera = GameObject.Find("Main Camera");
 
         scene1.SetActive(false);
         scene2.SetActive(false);
@@ -43,6 +71,10 @@ public class sceneManager : MonoBehaviour
 
         bubble1.SetActive(false);
         bubble2.SetActive(false);
+        bubble3.SetActive(false);
+        bubble4.SetActive(false);
+        bubble5.SetActive(false);
+        bubble6.SetActive(false);
         maskBubble.SetActive(false);
     }
 
@@ -58,6 +90,14 @@ public class sceneManager : MonoBehaviour
                 if (Input.GetMouseButtonDown(0))
                 {
                     phase = 1;
+
+                    bubble1.SetActive(true);
+                    bubble2.SetActive(true);
+                    maskBubble.SetActive(true);
+                    zeroBubble(maskBubble);
+                    zeroBubble(bubble1);
+                    zeroBubble(bubble2);
+                    scene1.SetActive(true);
                 }
                 break;
             
@@ -74,98 +114,128 @@ public class sceneManager : MonoBehaviour
                     phase = 2;
                     // set time to first bubble
                     trigger = Time.time + 0.5f;
+                    counter = 0;
                 }
                 break;
 
             // phase 2: bubble1 appears
             case 2:
 
-                if( Time.time > trigger)
+                if (counter > 50) growBubble(bubble1, bubble1InitScale);
+                if (counter > 100) growBubble(bubble2, bubble2InitScale);
+                if (counter > 150) growBubble(maskBubble, maskBubbleInitScale);
+
+                
+                counter++;
+
+                if (counter > 300)
                 {
-                    phase = 3;
-                    bubble1.SetActive(true);
-                    // set time to second bubble 
-                    trigger = Time.time + 0.5f;
-                }
-                break;
-
-            // phase 3: bubble 2 appears
-            case 3:
-
-                if (Time.time > trigger)
-                {
-                    phase = 4;
-                    bubble2.SetActive(true);
-                    // set time to mask-bubble 
-                    trigger = Time.time + 0.5f;
-                }
-
-                break;
-
-            // phase 4: mask bubble appears
-            case 4:
-
-                if (Time.time > trigger)
-                {
+                    counter = 0;
                     phase = 5;
-                    maskBubble.SetActive(true);
-                    scene1.SetActive(true);
                 }
-
                 break;
 
-                // phase 5: move things and grow mask bubble
+           // case 3?
+           // case 4?
+           // in no case were those ever important forreal
+
+
+                // phase 5: move camera into mask bubble
             case 5:
-                Vector3 pos3 = maskBubble.transform.position;
-                Vector3 scale = maskBubble.transform.localScale;
-                Vector3 pos4 = bubble1.transform.position;
-                Vector3 pos5 = bubble2.transform.position;
-                Vector3 pos6 = councillors.transform.position;
-                float deltaScale = 1.001f;
-                float bubbleDeltaX = 0.017f;
-                float bubbleDeltaY = 0.01f;
 
-                maskBubble.transform.position = new Vector3(pos3.x -0.05f, pos3.y-0.1f, pos3.z);
-                maskBubble.transform.localScale = new Vector3(scale.x* deltaScale, scale.y* deltaScale, scale.z* deltaScale);
-                bubble1.transform.position = new Vector3(pos4.x - bubbleDeltaX, pos4.y - bubbleDeltaY, pos4.z);
-                bubble2.transform.position = new Vector3(pos5.x + bubbleDeltaX, pos5.y - bubbleDeltaY, pos5.z);
-                councillors.transform.position = new Vector3(pos6.x, pos6.y - bubbleDeltaY, pos6.z);
+                Vector3 camDirection = camera.transform.position - new Vector3(-24.5f, 6.5f, -2f);
+                camera.transform.position -= camDirection.normalized * 0.01f * (Mathf.Sin(Mathf.PI*counter/1000f) *2f);
+                counter++;
 
-                if (maskBubble.transform.position.y < 3.5f) phase = 6;
+                if (camDirection.magnitude < 0.2f) phase = 6;
+
                 break;
 
+                // phase 6 is scene 1
             case 6:
-                Vector3 scale2 = maskBubble.transform.localScale;
-                Vector3 pos7 = maskBubble.transform.position;
-                float deltaScale2 = 1.002f;
-                maskBubble.transform.localScale = new Vector3(scale2.x * deltaScale2, scale2.y * deltaScale2, scale2.z * deltaScale2);
-                maskBubble.transform.position = new Vector3(pos7.x, pos7.y - 0.004f, pos7.z);
-
-                if (maskBubble.transform.localScale.x > 4f) phase = 7;
-
-                backdropFadeout = Mathf.Max(0f, backdropFadeout - 0.0015f);
-                backdrop.GetComponent<SpriteRenderer>().material.color = new Color(bgColor.r,bgColor.g,bgColor.b, backdropFadeout);
-                break;
-
-            case 7:
                 if (!scene1started)
                 {
                     scene1started = true;
+                    bubble1.SetActive(false);
+                    bubble2.SetActive(false);
                 }
                 else
                 {
                     if (scene1over)
                     {
                         scene1started = false;
-                        phase = 8;
+                        counter = 0;
+                        phase = 7;
                     }
                 }
                 break;
 
+                // transition back to council after scene 1
+            case 7:
+
+                Vector3 camDirection2 = camera.transform.position - new Vector3(-26.6f, 1f, -10f);
+                camera.transform.position -= camDirection2.normalized * 0.01f * (Mathf.Sin(Mathf.PI * counter / 1000f) * 2f);
+                counter++;
+
+                if (camDirection2.magnitude < 0.2f)
+                {
+                    phase = 8;
+                    counter = 0;
+
+                    bubble3.SetActive(true);
+                    bubble4.SetActive(true);
+
+                    zeroBubble(bubble3);
+                    zeroBubble(bubble4);
+                }
+
+                break;
+
+                // grow bubbles 3 and 4
             case 8:
+
+                shrinkBubble(maskBubble);
+
+                if (counter > 50) growBubble(bubble3, bubble3InitScale);
+                if (counter > 100) growBubble(bubble4, bubble4InitScale);
+                
+
+                counter++;
+
+                if(counter > 300)
+                {
+                    counter = 0;
+                    phase = 9;
+                }
+                break;
+
+            case 9:
 
                 break;
         }
+    }
+
+    void zeroBubble(GameObject input)
+    {
+
+        input.transform.localScale = new Vector3(0f, 0f, input.transform.localScale.z);
+    }
+    void shrinkBubble(GameObject input)
+    {
+        input.transform.localScale = new Vector3(
+                    Mathf.Max(0f, input.transform.localScale.x - 0.01f),
+                    Mathf.Max(0f, input.transform.localScale.y - 0.01f),
+                    input.transform.localScale.z
+                    );
+    }
+
+    void growBubble(GameObject input, Vector3 scale)
+    {
+        input.transform.localScale = new Vector3(
+                        Mathf.Min(scale.x, input.transform.localScale.x + 0.01f),
+                        Mathf.Min(scale.y, input.transform.localScale.y + 0.01f),
+                        input.transform.localScale.z
+                        );
     }
 
     
