19ba4b90f843e13575aeadd310a7ba4bbb783fa3
madslads <47368007+madslads@users.noreply.github.com>
2020-10-18 12:54:59 -0400

    characterScriptEdit

--- Gilgamesh/Assets/Maddi_H/breads.cs
+++ Gilgamesh/Assets/Maddi_H/breads.cs
@@ -4,16 +4,29 @@ using UnityEngine;
 
 public class breads : MonoBehaviour
 {
-    
+    private Sprite PackForest01_5,PackForest01_8;
+    private SpriteRenderer render;
     public int breadValue = 1;
 
+//private void Start()
+//    {
+  //      render = GetComponent<SpriteRenderer>();
+       // PackForest01_5 = Resources.Load<Sprite>("PackForest01_5");
+        //PackForest01_8 = Resources.Load<Sprite>("PackForest01_8");
+    //    render.sprite = PackForest01_5;
+    //}
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
            ScoreManager.instance.ChangeScore(breadValue);
+           if(breadValue.Equals(7))
+           {
+                 render.sprite = PackForest01_8;
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = PackForest01_8;
+           }
+        
         }
     }
-
 
 }
