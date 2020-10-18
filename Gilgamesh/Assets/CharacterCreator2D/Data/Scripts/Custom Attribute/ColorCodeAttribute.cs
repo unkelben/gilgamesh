cf2fc3928e39f6b0dad694ceb16737fb454c089d
sfba96 <31808176+sfba96@users.noreply.github.com>
2020-10-18 19:38:43 -0400

    ReImport Libraries

---
+++ Gilgamesh/Assets/CharacterCreator2D/Data/Scripts/Custom Attribute/ColorCodeAttribute.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CharacterCreator2D;
+
+public class ColorCodeAttribute : PropertyAttribute
+{
+}
--- Gilgamesh/Assets/Sebastian Beltran/CharacterCreator2D/Data/Scripts/Custom Attribute/ColorCodeAttribute.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using CharacterCreator2D;
-
-public class ColorCodeAttribute : PropertyAttribute
-{
-}
