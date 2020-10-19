2a1213687d716a4bbf237795651ba1344e6bb644
madslads <47368007+madslads@users.noreply.github.com>
2020-10-18 20:56:05 -0400

    titleScreenadded

---
+++ Gilgamesh/Assets/Maddi_H/destroy.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class destroy : MonoBehaviour {
+
+ [SerializeField]
+ GameObject objectToDestroy;
+
+ public void DestroyGameObject()
+ {
+  Destroy (objectToDestroy);
+ }
+}
