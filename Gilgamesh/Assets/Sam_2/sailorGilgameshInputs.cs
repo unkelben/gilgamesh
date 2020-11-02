bf8eabff6265ee8984da8e2003fe6a73dad695da
samjam <6parsam@gmail.com>
2020-11-01 19:11:24 -0500

    Started animating sail and gilgamesh

---
+++ Gilgamesh/Assets/Sam_2/sailorGilgameshInputs.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sailorGilgameshInputs : MonoBehaviour
+{
+    public List<Sprite> sprites;
+    public List<Vector2> displacement;
+    SpriteRenderer rend;
+    int height = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rend = gameObject.GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool changed = false;
+        if (Input.GetKeyDown("w"))
+        {
+            changed = true;
+            height = Mathf.RoundToInt(Mathf.Min(height + 1f, sprites.Count-1f));
+            Debug.Log("W! height: " + height);
+        }
+        else if (Input.GetKeyDown("s"))
+        {
+            changed = true;
+            height = Mathf.RoundToInt(Mathf.Max(height - 1f, 0f));
+            Debug.Log("S! height: " + height);
+        }
+
+        if (changed)
+        {
+            Debug.Log("update spriite " + height);
+            rend.sprite = sprites[height];
+            transform.position = new Vector3(displacement[height].x, displacement[height].y, transform.position.z);
+        }
+
+    }
+}
