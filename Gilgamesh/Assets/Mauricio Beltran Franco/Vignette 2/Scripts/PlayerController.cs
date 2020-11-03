9f4af8d500de542e75f2ed21b45b16a203f1f302
Mauricio Beltran Franco <mauricio.beltranfranco@live.fr>
2020-11-03 04:00:23 -0500

    Set up new input system

---
+++ Gilgamesh/Assets/Mauricio Beltran Franco/Vignette 2/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour {
+
+    Rigidbody2D rb;
+
+    [SerializeField] float speed;
+    Vector2 move;
+
+    // Start is called before the first frame update
+    void Start() {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+    }
+
+    void FixedUpdate() {
+        rb.MovePosition((Vector2)transform.position + move * speed * Time.fixedDeltaTime);
+    }
+
+    public void Move(InputAction.CallbackContext context) {
+        if (context.performed || context.canceled) {
+            move = context.ReadValue<Vector2>();
+            print(move);
+        }
+    }
+}
