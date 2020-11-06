1b7738f29bf042fef3d9c1181d57ed64ae965467
samjam <6parsam@gmail.com>
2020-11-05 23:02:48 -0500

    cool wind action

---
+++ Gilgamesh/Assets/Sam_2/windHandler.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class windHandler : MonoBehaviour
+{
+    // not really a reference for this script but it is where i got the line renderer idea
+    //https://www.gamasutra.com/blogs/JaneFriedhoff/20180402/316022/Where_C_Meets_CSS_Tech_Tricks_From_An_AR_Lyrics_Experiment.php
+    
+    public float startval1 = 0f;
+    public float startval2 = 50f;
+    public float endval1 = 50f;
+    public float endval2 = 100f;
+    float counter = 0;
+    public float animLength = 100;
+
+    public Material mat;
+    Material targetmat;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetmat = Instantiate(mat);
+        gameObject.GetComponent<LineRenderer>().material = targetmat;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float val1 = Mathf.Lerp(startval1, endval1, counter / animLength);
+        float val2 = Mathf.Lerp(startval2, endval2, counter / animLength);
+
+        targetmat.mainTextureOffset = new Vector2(val2, 0);
+        targetmat.mainTextureScale= new Vector2(val1, 1.19f);
+        counter = (counter + 1f) % animLength;
+    }
+}
