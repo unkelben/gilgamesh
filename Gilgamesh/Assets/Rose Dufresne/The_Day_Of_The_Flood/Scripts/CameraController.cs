bb0df3c9ffec523ce97962839fa8ee6ee8dd9ab8
RoseDuf <rose.dufresne@outlook.com>
2020-11-23 14:53:35 -0500

    added namespace for camera script

--- Gilgamesh/Assets/Rose Dufresne/The_Day_Of_The_Flood/Scripts/CameraController.cs
+++ Gilgamesh/Assets/Rose Dufresne/The_Day_Of_The_Flood/Scripts/CameraController.cs
@@ -2,95 +2,99 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-using Rose.Characters;
 
-public class CameraController : MonoBehaviour
+namespace Rose.Utilities
 {
-    GameObject target;
-    [SerializeField] float height; //above target
-    [SerializeField] float distance; //behind or in front of target
-    [SerializeField] float smoothingSpeed;
+    using Rose.Characters;
 
-    private Vector3 refVelocity;
-    private float refSpeed;
-    private float fixedLookAtRotation;
-
-    float currentHeight;
-
-    private Camera playerCamera;
-        
-    void Start()
+    public class CameraController : MonoBehaviour
     {
-        target = GameObject.FindGameObjectWithTag("Player");
-        playerCamera = GetComponent<Camera>();
-        currentHeight = this.GetComponent<Camera>().orthographicSize;
-    }
-        
-    void Update()
-    {
-        HandleCamera();
-    }
+        GameObject target;
+        [SerializeField] float height; //above target
+        [SerializeField] float distance; //behind or in front of target
+        [SerializeField] float smoothingSpeed;
 
-    protected virtual void HandleCamera()
-    {
-        float mainHeight = 2f * Camera.main.orthographicSize;
-        float mainWidth = mainHeight * Camera.main.aspect;
+        private Vector3 refVelocity;
+        private float refSpeed;
+        private float fixedLookAtRotation;
 
-        float height = 2f * this.GetComponent<Camera>().orthographicSize;
-        float width = height * this.GetComponent<Camera>().aspect;
+        float currentHeight;
 
-        //move camera position to follow target
-        float xPosition = 0;
-        float yPosition = 0;
+        private Camera playerCamera;
 
-        Vector3 targetPosition = new Vector3(xPosition, yPosition, transform.position.z);
-
-        if (!target)
+        void Start()
         {
-            playerCamera.orthographicSize = Mathf.SmoothDamp(height / 2, mainHeight / 2, ref refSpeed, 1);
-            smoothingSpeed = 1;
+            target = GameObject.FindGameObjectWithTag("Player");
+            playerCamera = GetComponent<Camera>();
+            currentHeight = this.GetComponent<Camera>().orthographicSize;
         }
-        else
+
+        void Update()
         {
-            if ((target.transform.position.x - width / 2 <= -mainWidth / 2) || (target.transform.position.x + width / 2 >= mainWidth / 2))
-            {
-                xPosition = transform.position.x;
-            }
-            else
-            {
-                xPosition = target.transform.position.x;
-            }
-            if ((target.transform.position.y - height / 2 <= -mainHeight / 2) || (target.transform.position.y + height / 2 >= mainHeight / 2))
-            {
-                yPosition = transform.position.y;
-            }
-            else
-            {
-                yPosition = target.transform.position.y;
-            }
+            HandleCamera();
+        }
 
-            targetPosition = new Vector3(xPosition, yPosition, transform.position.z);
+        protected virtual void HandleCamera()
+        {
+            float mainHeight = 2f * Camera.main.orthographicSize;
+            float mainWidth = mainHeight * Camera.main.aspect;
 
-            //zoom in/out
-            if (target.GetComponent<PlayerController>().inBoat)
+            float height = 2f * this.GetComponent<Camera>().orthographicSize;
+            float width = height * this.GetComponent<Camera>().aspect;
+
+            //move camera position to follow target
+            float xPosition = 0;
+            float yPosition = 0;
+
+            Vector3 targetPosition = new Vector3(xPosition, yPosition, transform.position.z);
+
+            if (!target)
             {
                 playerCamera.orthographicSize = Mathf.SmoothDamp(height / 2, mainHeight / 2, ref refSpeed, 1);
+                smoothingSpeed = 1;
             }
             else
             {
-                playerCamera.orthographicSize = Mathf.SmoothDamp(height / 2, currentHeight, ref refSpeed, 1);
+                if ((target.transform.position.x - width / 2 <= -mainWidth / 2) || (target.transform.position.x + width / 2 >= mainWidth / 2))
+                {
+                    xPosition = transform.position.x;
+                }
+                else
+                {
+                    xPosition = target.transform.position.x;
+                }
+                if ((target.transform.position.y - height / 2 <= -mainHeight / 2) || (target.transform.position.y + height / 2 >= mainHeight / 2))
+                {
+                    yPosition = transform.position.y;
+                }
+                else
+                {
+                    yPosition = target.transform.position.y;
+                }
+
+                targetPosition = new Vector3(xPosition, yPosition, transform.position.z);
+
+                //zoom in/out
+                if (target.GetComponent<PlayerController>().inBoat)
+                {
+                    playerCamera.orthographicSize = Mathf.SmoothDamp(height / 2, mainHeight / 2, ref refSpeed, 1);
+                }
+                else
+                {
+                    playerCamera.orthographicSize = Mathf.SmoothDamp(height / 2, currentHeight, ref refSpeed, 1);
+                }
             }
-        }
 
-        Vector3 finalPosition = targetPosition;
+            Vector3 finalPosition = targetPosition;
 
-        if ((int)height / 2 == currentHeight)
-        {
-            transform.position = Vector3.SmoothDamp(transform.position, finalPosition, ref refVelocity, smoothingSpeed);
+            if ((int)height / 2 == currentHeight)
+            {
+                transform.position = Vector3.SmoothDamp(transform.position, finalPosition, ref refVelocity, smoothingSpeed);
+            }
+            else
+                if (height / 2 - 40 >= 0)
+                transform.position = Vector3.SmoothDamp(transform.position, finalPosition, ref refVelocity, (height / 2 - 40) / 20);
+            //transform.LookAt(Vector3.forward, target.position);
         }
-        else
-            if (height / 2 - 40 >= 0)
-                transform.position = Vector3.SmoothDamp(transform.position, finalPosition, ref refVelocity, (height/2-40)/20);
-        //transform.LookAt(Vector3.forward, target.position);
     }
 }
